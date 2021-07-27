cffd0d4dfcf5469d94f2d69beaa80597d2ed06a6
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2021-07-26 21:29:03 -0300

    Atualização conexao

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,14 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'database1270',
-    database: 'ong_adote_ja',
-    port: 5432
+    host: 'ec2-52-72-125-94.compute-1.amazonaws.com',
+    user: 'qulxzosjrxaaim',
+    password: 'fa6799e152ec2daf05b7a5c9ead4173da09e8d42c9cb7a59008d45d3a80b3694',
+    database: 'd3shpkuerga8ia',
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
